2b29c69333d683d8925443f5a19f7e4e6410e291
CovChEGG <covchegg@gmail.com>
2022-02-03 00:39:11 +0700

    ...not last changes

--- dev1Group6/Program.cs
+++ dev1Group6/Program.cs
@@ -1,1 +1,40 @@
 ﻿
+void PrintArrayInt(int[] array)
+{
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[] CreateArray(int numberOfElements, int min, int max)
+{
+    int[] array = new int[numberOfElements];
+    Random rnd = new Random();
+    for (int i = 0; i < numberOfElements; i++)
+        {
+            array[i, i] = rnd.Next(min, max);
+        }
+    return array;
+}
+
+int[] CreateEvenNumbersArray(int[] array)
+{
+    int[] outArray = new int[0];
+    for (int i = 0; i < array.length; i++)
+    {
+        if(array[i]%2!=0) continue;
+        else
+        {
+            Array.Resize(ref outArray, 1)
+            
+        }
+    }
+}
+
+var array = CreateArray(10 , -10, 10);
+PrintArrayInt(array);
+CreateEvenNumbersArray(int[] array)
